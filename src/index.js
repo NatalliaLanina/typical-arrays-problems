253ef02f249aa461a3d38b1201cc2accc63725c3
Natallia Lanina <natalielanina@yahoo.com>
2021-08-01 18:10:53 +0300

    docs: do typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ( arguments.length == 0 || array.length == 0 ) {
+    return 0;
+  };
+  let result = Math.min.apply(null, array);
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ( arguments.length == 0 || array.length == 0) {
+    return 0;
+  };
+  let result = Math.max.apply(null, array);
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ( arguments.length == 0 || array.length == 0) {
+    return 0;
+  }
+  return array.reduce((a, b) => (a + b)) / array.length;
 }
